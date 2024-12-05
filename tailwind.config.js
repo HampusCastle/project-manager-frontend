05a37685d3af8dd5468f3121243427cc581d0e74
HampusCastle <Hampuscastle@gmail.com>
2024-12-05 15:17:48 +0100

    uppdatering av css, borttagning av filer, adderande av dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  './src/**/*.{html,js,jsx,ts,tsx}', 
+];
 
+export const theme = {
+  extend: {
+    colors: {
+      primary: 'var(--primary-color)',       
+      secondary: 'var(--secondary-color)',   
+      bg: 'var(--bg-color)',                
+      text: 'var(--text-color)',             
+      border: 'var(--border-color)',         
+      highlight: 'var(--highlight-color)',   
+      taskCardBg: 'var(--task-card-bg)',     
+      statusTodo: 'var(--status-todo)',     
+      statusInProgress: 'var(--status-in-progress)', 
+      statusCompleted: 'var(--status-completed)', 
+    },
+  },
+};
+
+export const plugins = [];
